948e0b7bc3e0f0e7d2ae97021a1327dad60c5f8c
jogli5er <robrunner@student.ethz.ch>
2018-04-10 00:33:00 +0200

    Easily pass init urls to docker container
    These changes were necessary to easily pass a init
    file to the container.

--- Run.sh
+++ Run.sh
@@ -5,15 +5,18 @@ set -o pipefail
 set -o nounset
 
 arg=${1:-}
-export tdse_server_start_command="npm start"
-if [ "$arg" = "test" ]; then
-	printf "Starting containers for testing"
+export tdse_server_start_command="npm start -i /home/node/init_urls.csv"
+if [ "$arg" = "shell" ]; then
+	printf "Starting containers for direct manipulation over shell\n"
 	export tdse_server_start_command="/bin/bash"
 elif [ "$arg" = "debug" ]; then
-	printf "Starting containers in debug mode"
-	export tdse_server_start_command="npm run debug"
+	printf "Starting containers in debug mode\n"
+	export tdse_server_start_command="npm run debug -i /home/node/init_urls.csv"
+elif [ "$arg" = "test" ]; then
+	printf "Starting container in testing mode\n"
+	export tdse_server_start_command="npm test"
 else
-	printf "Starting containers in productive mode"
+	printf "Starting containers in productive mode\n"
 fi
 
 printf "Current value of tdse_server_start_command: %s\n" "$tdse_server_start_command"
--- Setup.sh
+++ Setup.sh
@@ -5,5 +5,5 @@ set -o pipefail
 set -o nounset
 
 
-docker build --rm -t robrunne/tdse_dependencies:1.0.0 deployment/
+docker build --rm -t robrunne/tdse-dependencies:1.0.0 deployment/
 docker build -t robrunne/tdse-spider:1.0.0 server/
