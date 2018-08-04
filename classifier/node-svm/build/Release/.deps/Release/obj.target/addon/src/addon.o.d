3e67364d5c522b09849fed0a1d3e12d49ceed68a
jogli5er <robrunner@student.ethz.ch>
2018-08-05 01:13:49 +0200

    Build folder misused by node-svm - .gitingore interfered
    It did not build on keftes nor tik due to the fact that a directory was
    missing. It was missing due to our gitignore, which factually is
    correct, however the node-svm module does things slightly differently
    and has a build/Release directory for some of its dependencies.
    Solution: force add this to the repo

---
+++ classifier/node-svm/build/Release/.deps/Release/obj.target/addon/src/addon.o.d
@@ -0,0 +1,63 @@
+cmd_Release/obj.target/addon/src/addon.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/robrunne/.node-gyp/8.11.3/include/node -I/home/robrunne/.node-gyp/8.11.3/src -I/home/robrunne/.node-gyp/8.11.3/deps/uv/include -I/home/robrunne/.node-gyp/8.11.3/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -O3 -fPIC -c -O3 -fno-omit-frame-pointer -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/addon/src/addon.o.d.raw   -c -o Release/obj.target/addon/src/addon.o ../src/addon.cc
+Release/obj.target/addon/src/addon.o: ../src/addon.cc \
+ ../src/node-svm/node-svm.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/v8.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/v8-version.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/v8config.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node_version.h \
+ ../node_modules/nan/nan.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node_version.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv-errno.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv-version.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv-unix.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv-threadpool.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/uv-linux.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node_buffer.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node.h \
+ /home/robrunne/.node-gyp/8.11.3/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h ../src/node-svm/../libsvm/svm.h
+../src/addon.cc:
+../src/node-svm/node-svm.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/v8.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/v8-version.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/v8config.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node_version.h:
+../node_modules/nan/nan.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node_version.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv-errno.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv-version.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv-unix.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv-threadpool.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/uv-linux.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node_buffer.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node.h:
+/home/robrunne/.node-gyp/8.11.3/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../src/node-svm/../libsvm/svm.h:
---
+++ classifier/node-svm/build/Release/.deps/Release/obj.target/addon/src/libsvm/svm.o.d
@@ -0,0 +1,5 @@
+cmd_Release/obj.target/addon/src/libsvm/svm.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/robrunne/.node-gyp/8.11.3/include/node -I/home/robrunne/.node-gyp/8.11.3/src -I/home/robrunne/.node-gyp/8.11.3/deps/uv/include -I/home/robrunne/.node-gyp/8.11.3/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -O3 -fPIC -c -O3 -fno-omit-frame-pointer -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/addon/src/libsvm/svm.o.d.raw   -c -o Release/obj.target/addon/src/libsvm/svm.o ../src/libsvm/svm.cpp
+Release/obj.target/addon/src/libsvm/svm.o: ../src/libsvm/svm.cpp \
+ ../src/libsvm/svm.h
+../src/libsvm/svm.cpp:
+../src/libsvm/svm.h:
