d175d76379462b753f1fc890880a5c77aacb7ce0
jogli5er <robrunner@student.ethz.ch>
2020-06-18 10:53:05 +0200

    Fixed an issue in Setup.sh
    Forgot about a base image for data preprocessing. Added it to the setup
    script.

--- Setup.sh
+++ Setup.sh
@@ -7,4 +7,5 @@ set -o nounset
 
 docker build --rm -t robrunne/tdse-py-dep:1.0.0 -f deployment/pythonBase/Dockerfile .
 docker build --rm -t robrunne/tdse-dependencies:1.0.0 -f deployment/Dockerfile .
+docker build --rm -t robrunne/tdse-prepbase:1.0.0 -f dataPreprocessing/baseImage/Dockerfile .
 docker build -t robrunne/tdse-spider:1.0.0 server/
